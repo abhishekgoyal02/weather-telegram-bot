f6c6165088cbbb620d699ebcff4d87c3a8b30eab
Abhishek Goyal <coder02.error404@gmail.com>
2026-04-13 11:09:27 +0530

    Fix deployment issues and port binding

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import os
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-load_dotenv()
+from telegram.ext import CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder
 from weather import get_weather, will_rain_next_12_hours
 from alerts import send_rain_alert, users
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    server = HTTPServer(("0.0.0.0", 10000), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     users.add(chat_id)
-
     await update.message.reply_text(
         "You will now receive automatic rain alerts!\nUse /weather <city>"
     )
-
 
 async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
@@ -44,7 +55,6 @@ async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("weather", weather))
 app.job_queue.run_repeating(send_rain_alert, interval=3600, first=10)
